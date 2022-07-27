4a75d6afdaebdad8ffa1bab48fb6f5c9557558ac
Lucy Kendall <lucykendall88@gmail.com>
2022-07-27 23:35:57 +1000

    confirmed js is working

--- static/formvalidation.js
+++ static/formvalidation.js
@@ -1,10 +1,10 @@
 function validateForm() {
     console.log('here')
-    let x = document.getElementById("values").value;
-        print(x)
-    if (x == "") {
-        alert("Input fields must be filled out");
-        return false;
-        }
+    // let x = document.getElementById("values").value;
+    //     print(x)
+    // if (x == "") {
+    //     alert("Input fields must be filled out");
+    //     return false;
+    //     }
     }
 validateForm()
